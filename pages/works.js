4542a4cb567524fd3835e1b45433920af420c6cf
Jeron <jeronokkers7@gmail.com>
2023-07-05 03:00:26 +0200

    Initial commit

--- pages/works.js
+++ pages/works.js
@@ -1,12 +1,10 @@
-import { Container, Divider, Heading, SimpleGrid } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 import Layout from '../components/layouts/article'
 
 import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
 import thumbMenkiki from '../public/images/works/menkiki_eyecatch.png'
-import thumbTour from '../public/images/works/modetokyo_eyecatch.png'
-import thumbStyly from '../public/images/works/styly_eyecatch.png'
 import thumbPortfolio from '../public/images/works/portfolio.png'
 
 const Works = () => {
